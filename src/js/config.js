94f4d5735dedad649f54203253bda9f2d88cf036
Jakub Kozanecki <jakubkozaneckidev@gmail.com>
2022-09-22 17:59:50 +0200

    Initzial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "b9f7e3d8-c0d0-43a6-9ba2-d3ab5b68bbc1";
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/searchView.js
@@ -0,0 +1,21 @@
+class SearchView {
+  _parentEl = document.querySelector(".search");
+
+  getQuery() {
+    return this._parentEl.querySelector(".search__field").value;
+  }
+
+  _clearInput() {
+    this._parentEl.querySelector(".search__field").value = "";
+  }
+
+  addHandlerSearch(handler) {
+    this._parentEl.addEventListener("submit", (e) => {
+      e.preventDefault();
+      handler();
+      this._clearInput();
+    });
+  }
+}
+
+export default new SearchView();
